15fc2826ccb5b1899e233bf6960c544012563c8f
Denival Biotto Filho <denivaldenival@gmail.com>
2026-04-30 19:17:01 -0300

    Create sistema_playlist.py

---
+++ sistema_playlist.py
@@ -0,0 +1,7 @@
+class Musica:
+    def __init__(self, id, titulo, artista, genero, bpm):
+        self.id = id
+        self.titulo = titulo
+        self.artista = artista
+        self.genero = genero
+        self.bpm = bpm
